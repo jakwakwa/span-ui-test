a6baa2bbc1bbef3e925e11e5b1ce3d063a586535
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-06-14 20:56:53 +0200

    header component fix

--- src/app/templates/base/components/header/header.component.ts
+++ src/app/templates/base/components/header/header.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss']
 })
+
 export class HeaderComponent {
   logo = 'assets/img/dummy-logo.svg';
   profileImg = 'assets/img/profile-img.jpg';
