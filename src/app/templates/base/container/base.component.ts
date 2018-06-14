8b7f044692db2c4570fe2d85ea2f65ba26887899
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-06-14 21:28:30 +0200

    base component cleaned

--- src/app/templates/base/container/base.component.ts
+++ src/app/templates/base/container/base.component.ts
@@ -19,6 +19,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   ]
 })
 export class BaseComponent implements OnInit {
+
   menuState = 'out';
 
   projects: any[];
