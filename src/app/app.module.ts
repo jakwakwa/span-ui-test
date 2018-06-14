fc66b81550e3163429253dd15c5a875ac2565879
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-06-14 23:10:07 +0200

    projects background set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   // add this (above) again after work complete
   { path: '**', component: NotFoundComponent }
 ];
