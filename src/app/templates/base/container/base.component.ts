92470da7867c2f10ab90909c450cdb0be9cb0189
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-06-14 18:09:18 +0200

    menu finished on mobile

--- src/app/templates/base/container/base.component.ts
+++ src/app/templates/base/container/base.component.ts
@@ -11,7 +11,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         transform: 'translate3d(0, 0, 0)'
       })),
       state('out', style({
-        transform: 'translate3d(-100%, 0, 0)'
+        transform: 'translate3d(-200%, 0, 0)'
       })),
       transition('in => out', animate('400ms ease-in-out')),
       transition('out => in', animate('400ms ease-in-out'))
@@ -19,7 +19,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   ]
 })
 export class BaseComponent implements OnInit {
-  // menuState = 'out';
+  menuState = 'out';
 
   projects: any[];
 
